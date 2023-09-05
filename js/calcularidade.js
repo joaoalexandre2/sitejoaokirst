7a29da550520821b0bf81724bf1289b4421fca25
João Alexandre Kirst <joaoalekirst@hotmail.com>
2023-09-05 15:40:11 -0300

    Commit alteração calcular idade e anos que estou trabalhando como programador

--- js/calcularidade.js
+++ js/calcularidade.js
@@ -1,29 +1,22 @@
 
-  // Data de nascimento no formato "MM/DD/YYYY"
-  const dataNascimento = new Date("01/01/1995");
-  
+
+  // Função para calcular a quantidade de anos trabalhados   // Função para calcular a idade
+function calcularIdadeEAnosTrabalhados(dataNascimento, dataInicioTrabalho) {
+  const hoje = new Date();
+  const milissegundosNoAno = 365.25 * 24 * 60 * 60 * 1000;
+
+  const idade = Math.floor((hoje - dataNascimento) / milissegundosNoAno);
+  const anosTrabalhando = Math.floor((hoje - dataInicioTrabalho) / milissegundosNoAno);
+
+  return { idade, anosTrabalhando };
+}
+ // Data de nascimento no formato "MM/DD/YYYY"
+const dataNascimento = new Date("19/10/1994");
   // Data de início de trabalho na área no formato "MM/DD/YYYY"
-  const dataInicioTrabalho = new Date("01/01/2020");
+const dataInicioTrabalho = new Date("01/01/2020");
 
-  // Função para calcular a idade
-  function calcularIdade(dataNascimento) {
-    const hoje = new Date();
-    const diff = hoje - dataNascimento;
-    const idade = Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
-    return idade;
-  }
+const { idade, anosTrabalhando } = calcularIdadeEAnosTrabalhados(dataNascimento, dataInicioTrabalho);
 
-  // Função para calcular a quantidade de anos trabalhados
-  function calcularAnosTrabalhando(dataInicioTrabalho) {
-    const hoje = new Date();
-    const diff = hoje - dataInicioTrabalho;
-    const anosTrabalhando = Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
-    return anosTrabalhando;
-  }
-
-  // Atualizar os elementos HTML com a idade e a quantidade de anos trabalhados calculados
-  const idadeElement = document.getElementById("idade");
-  const anosTrabalhandoElement = document.getElementById("anosTrabalhando");
-  idadeElement.textContent = calcularIdade(dataNascimento);
-  anosTrabalhandoElement.textContent = calcularAnosTrabalhando(dataInicioTrabalho);
+document.getElementById("idade").textContent = idade;
+document.getElementById("anosTrabalhando").textContent = anosTrabalhando
 
